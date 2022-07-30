d2cdb9c065a602a0dc55b042ddb914ee1550e3c5
Sean <seanzhu@Seans-Mac-mini.local>
2022-07-29 18:16:08 -0600

    Sidebar and navbar

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ import { Browser } from '@syncfusion/ej2/base'
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components'
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages'
 
+import { useStateContext } from './contexts/ContextProvider'
+
+import './App.css'
 
 const App = () => {
-
-  const activeMenu = false 
+  const { activeMenu } = useStateContext() 
 
   return (
     <div>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import App from './App' 
-import './App.css'
+import './App.css' 
+import { ContextProvider } from './contexts/ContextProvider';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+    <ContextProvider>
+        <App />
+    </ContextProvider>,
+    document.getElementById('root'))
