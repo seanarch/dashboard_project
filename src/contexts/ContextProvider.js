fd08c3b9f44565263ed690632de98a06b7f8cfc0
Sean <seanzhu@Seans-Mac-mini.local>
2022-07-30 14:56:30 -0600

    Update Navbar component

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -9,12 +9,19 @@ const initialState = {
     notification: false,
 }
 
+ 
 export const ContextProvider = ({ children }) => {
     const [activeMenu, setActiveMenu] = useState(true)
+    const [isClicked, setIsClicked] = useState(initialState)
+    const [screenSize, setScreenSize] = useState(undefined)
+
+    const handleClick = (clicked) => {
+        setIsClicked({...initialState, [clicked]:true})
+    }
 
     return (
         <StateContext.Provider
-            value={{activeMenu, setActiveMenu}}>
+            value={{activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenSize, setScreenSize}}>
                 {children}
         </StateContext.Provider>
     )
