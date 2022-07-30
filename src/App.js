177b637612636b5c9fa4f31bc75501449768e192
Sean <seanzhu@Seans-Mac-mini.local>
2022-07-30 15:25:53 -0600

    Update ecommerce page

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ const App = () => {
                 <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
                     <Navbar />
                     </div>
-                </div>
+                
                 <div>
                     <Routes>
                         {/* Dashboard */}
@@ -73,6 +73,7 @@ const App = () => {
                         <Route path="/stacked" element={<Stacked />} />
 
                     </Routes>
+                </div>
                 </div>
             </div>
         </BrowserRouter>
